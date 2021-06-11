a02692dfa9021e4c094a805511291ef398ae9eb3
AndreLarson <dre.larson7@gmail.com>
2021-06-11 15:42:38 -0700

    setup basic java fx files

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,24 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/base.fxml")));
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Tetris by Andre Larson");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
