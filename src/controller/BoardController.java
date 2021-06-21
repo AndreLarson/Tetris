873ee524506bab6f77b7689ce99a7e237ef30808
AndreLarson <dre.larson7@gmail.com>
2021-06-21 15:43:04 -0700

    got shapes to show in the gui

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -1,38 +1,72 @@
 package controller;
 
-import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
-import javafx.scene.input.KeyCode;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Board;
 
-import static javafx.scene.layout.Priority.SOMETIMES;
-
 public class BoardController {
 
+    private static int BOARD_WIDTH = 350;
+
+    private static int BOARD_HEIGHT = 600;
+
+    private static int RECT_WIDTH = BOARD_WIDTH / (Board.BOARD_COLUMNS);
+
+    private static int RECT_HEIGHT = BOARD_HEIGHT / (Board.BOARD_ROWS - 4);
+
+    private static int BORDER_WIDTH = 3;
+
     @FXML
-    private GridPane myGridPane;
+    private Canvas myCanvas;
+
+    private GraphicsContext myGraphics;
 
     private Board myBoard;
-
-    private AnimationTimer myTimer;
 
     private Stage myStage;
 
     public BoardController() {
         myBoard = new Board();
-        myTimer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                // want one sec delay
-                myBoard.step();
+    }
+
+    @FXML
+    public void initialize() {
+        myCanvas.setHeight(BOARD_HEIGHT);
+        myCanvas.setWidth(BOARD_WIDTH);
+        myGraphics = myCanvas.getGraphicsContext2D();
+        for (int i = 0; i < RECT_HEIGHT * (Board.BOARD_ROWS - 4); i += RECT_HEIGHT) {
+            for (int j = 0; j < RECT_WIDTH * Board.BOARD_COLUMNS; j += RECT_WIDTH) {
+                drawSquare(j, i, false);
             }
-        };
+        }
+    }
+
+    private void drawSquare(int j, int i, boolean theValue) {
+        myGraphics.setFill(Color.BLACK);
+        myGraphics.fillRect(j, i, RECT_WIDTH, RECT_HEIGHT);
+        if (!theValue) {
+            myGraphics.setFill(Color.WHITE);
+        } else {
+            myGraphics.setFill(Color.ORANGE);
+        }
+        myGraphics.fillRect(j + BORDER_WIDTH, i + BORDER_WIDTH, RECT_WIDTH - (BORDER_WIDTH * 2), RECT_HEIGHT - (BORDER_WIDTH * 2));
+    }
+
+    private void updateGUI() {
+        for (int i = 0; i < Board.BOARD_ROWS - 4; i++) {
+            for (int j = 0; j < Board.BOARD_COLUMNS; j++) {
+                if (myBoard.getBoard()[i + 4][j]) {
+                    drawSquare(j * RECT_WIDTH, i * RECT_HEIGHT, true);
+                    System.out.println("happened");
+                } else {
+                    drawSquare(j * RECT_WIDTH, i * RECT_HEIGHT, false);
+                }
+            }
+        }
     }
 
     public void setStage(Stage theStage) {
@@ -43,7 +77,6 @@ public class BoardController {
                     //start
                     //if started stop
                     myBoard.start();
-                    updateGUI();
                     System.out.println("works");
                     break;
                 case UP:
@@ -71,44 +104,4 @@ public class BoardController {
         });
     }
 
-    @FXML
-    public void initialize() {
-        for (int i = 0, j = 0; i < Board.BOARD_ROWS - 4; i++) {
-            if (j < Board.BOARD_COLUMNS) {
-                ColumnConstraints column = new ColumnConstraints();
-                column.setHgrow(SOMETIMES);
-                myGridPane.getColumnConstraints().add(column);
-            }
-            RowConstraints row = new RowConstraints();
-            row.setVgrow(SOMETIMES);
-            myGridPane.getRowConstraints().add(row);
-        }
-        for (int i = 0; i < Board.BOARD_ROWS - 4; i++) {
-            for (int j = 0; j < Board.BOARD_COLUMNS; j++) {
-                Pane pane = new Pane();
-                if (myBoard.getBoard()[i + 4][j] == true) {
-                    pane.setStyle("-fx-border-color: #000000;" + "-fx-border-width: 2.5;" + "-fx-background-color: #d30000");
-                    System.out.println("happened");
-                } else {
-                    pane.setStyle("-fx-border-color: #000000;" + "-fx-border-width: 2.5;" + "-fx-background-color: #ffffff");
-                }
-                myGridPane.add(pane, j, i);
-            }
-        }
-    }
-
-    private void updateGUI() {
-        for (int i = 0; i < Board.BOARD_ROWS - 4; i++) {
-            for (int j = 0; j < Board.BOARD_COLUMNS; j++) {
-                Pane pane = new Pane();
-                if (myBoard.getBoard()[i + 4][j] == true) {
-                    pane.setStyle("-fx-border-color: #000000;" + "-fx-border-width: 2.5;" + "-fx-background-color: #d30000");
-                    System.out.println("happened");
-                } else {
-                    pane.setStyle("-fx-border-color: #000000;" + "-fx-border-width: 2.5;" + "-fx-background-color: #ffffff");
-                }
-                myGridPane.getChildren().add(i, pane);
-            }
-        }
-    }
 }
--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,19 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
         FXMLLoader baseLoader = new FXMLLoader(getClass().getResource("/view/base.fxml"));
-        FXMLLoader boardLoader = new FXMLLoader(getClass().getResource("/view/board.fxml"));
         Parent baseRoot = (Parent) baseLoader.load();
-        boardLoader.load();
-        Scene scene = new Scene(baseRoot, 600, 900);
-        BoardController boardController = boardLoader.getController();
+        BoardController boardController = baseLoader.getController();
         boardController.setStage(primaryStage);
+        Scene scene = new Scene(baseRoot, 600, 900);
         primaryStage.setTitle("Tetris by Andre Larson");
         primaryStage.setScene(scene);
         primaryStage.show();
